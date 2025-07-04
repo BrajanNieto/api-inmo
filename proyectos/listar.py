4e74102c2bdf67d633204ad600d4c94159846a47
BrajanNieto <brajan.nieto@utec.edu.pe>
2025-07-04 16:42:21 -0500

    Update listar.py

--- proyectos/listar.py
+++ proyectos/listar.py
@@ -1,12 +1,15 @@
+# proyectos/listar.py
+
 from shared.utils import dynamodb, dumps
 
-TABLE = dynamodb.Table("clientes")
+TABLE = dynamodb.Table("proyectos")
 
 def lambda_handler(event, context):
     items = []
     # Scan paginado para evitar timeouts en tablas grandes
     response = TABLE.scan()
     items.extend(response.get("Items", []))
+
     while "LastEvaluatedKey" in response:
         response = TABLE.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
         items.extend(response.get("Items", []))
